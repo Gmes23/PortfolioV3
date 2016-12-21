774de7131881de598608a91bb688f8fdb72b753a
Gerardo Mestanza <gmesda23@gmail.com>
2016-12-20 19:21:49 -0500

    added typography

--- public/assets/gmJs/gmMainJS.js
+++ public/assets/gmJs/gmMainJS.js
@@ -23,3 +23,15 @@ $('.grid').masonry({
   // options...
   itemSelector: '.grid-item'
 });
+
+function resizeImage(){
+  var image = jQuery("img");
+  var imageParentElement = jQuery('.col-md-12');
+  var widthRatio = imageParentElement.outerWidth() / imageRatio.width;
+  var heightRatio = imageParentElement.outerHeight() / imageRatio.height;
+  var ratio = Math.max(widthRatio, heightRatio);
+  image.css({
+    width: Math.ceil(ratio*imageRatio.width) ,
+    height: Math.ceil(ratio*imageRatio.height)
+  });
+}
