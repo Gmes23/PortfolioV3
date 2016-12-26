025acd055ddb079f95a1505399dff061d720d6ab
Gmes23 <gmesda23@gmail.com>
2016-12-25 23:40:27 -0500

    backup commit

--- public/assets/gmJs/gmMainJS.js
+++ public/assets/gmJs/gmMainJS.js
@@ -1,35 +1,7 @@
 
-// $('.gmMenuLogo').click(function(e){
-//   e.preventDefault();
-// $(".gmNav").animate({width:'toggle'},600);
-// $(".gmMenuLogo").css('display', 'none');
-//
-// });
-//
-// $('.gmReopen').click(function(e){
-//   e.preventDefault();
-// $(".gmNav").animate({width:'toggle'},600);
-// $(".gmMenuLogo").css('display', 'block');
-//
-// });
-
-
 $('.gmMenuLogo').on('click', function() {
     $('.gmNav').toggleClass('open');
     $('.gmMenuLogo').toggleClass('open');
     $('.gmMenuTop').toggleClass('open2');
     $('.gmMenuBot').toggleClass('open3');
 });
-
-
-// function resizeImage(){
-//   var image = jQuery("img");
-//   var imageParentElement = jQuery('.col-md-12');
-//   var widthRatio = imageParentElement.outerWidth() / imageRatio.width;
-//   var heightRatio = imageParentElement.outerHeight() / imageRatio.height;
-//   var ratio = Math.max(widthRatio, heightRatio);
-//   image.css({
-//     width: Math.ceil(ratio*imageRatio.width) ,
-//     height: Math.ceil(ratio*imageRatio.height)
-//   });
-// }
